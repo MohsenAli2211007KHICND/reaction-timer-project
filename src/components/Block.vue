b865ca718b21bc8736d8abad81a5e538113ade73
Mohsen Ali <mohsen.ali@xloopdigital.com>
2023-06-02 15:06:32 +0500

    LifeCycle Hooks Added to Block Component

---
+++ src/components/Block.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="block" v-if="showBlock">
+    Click me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data () {
+        return {
+            showBlock: false
+        }
+    },
+    mounted () {
+        console.log('Mouted Block component');
+        setTimeout(() => {
+            this.showBlock = true
+            console.log(this.delay);
+        }, this.delay);
+    },
+    updated () {
+        console.log("showBlock Attribut Updated!");
+    }
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
